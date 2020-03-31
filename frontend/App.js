c99e05e1bd48845a77750dc4016e3c8b4d41368e
alkrepp <alkrepp@stud.hs-heilbronn.de>
2020-03-31 18:44:31 +0200

    code refactoring: HomeView, HomeViewStyle and App

--- frontend/App.js
+++ frontend/App.js
@@ -1,73 +1,22 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-
-const Home = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Button" onPress={() => navigation.navigate("Kalender")} />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
-
-// const BottomTab = createBottomTabNavigator();
-
-// const Root = () => {
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name="Schreibtisch" component={Home} />
-//       <Tab.Screen name="Kalender" component={CalenderScreen} />
-//       <Tab.Screen name="Studenplan" component={Home}/>
-//       <Tab.Screen name="Wichtige Links" component={Home}/>
-//       <Tab.Screen name="Fächer" component={Home}/>
-//       <Tab.Screen name="Noten" component={Home}/>
-//       <Tab.Screen name="Lernplan" component={Home}/>
-//     </Tab.Navigator>
-//   );
-// }
-
-const Stack = createStackNavigator();
-
-const Root = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Schreibtisch" component={Home} />
-      <Stack.Screen name="Kalender" component={Home} />
-      <Stack.Screen name="Studenplan" component={Home} />
-      <Stack.Screen name="Wichtige Links" component={Home} />
-      <Stack.Screen name="Fächer" component={Home} />
-      <Stack.Screen name="Noten" component={Home} />
-      <Stack.Screen name="Lernplan" component={Home} />
-    </Stack.Navigator>
-  );
-};
+import HomeView from "./src/views/HomeView";
+import CalenderView from "./src/views/CalenderView";
 
 const Drawer = createDrawerNavigator();
 
-export default function Menu() {
+export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Root">
-        <Drawer.Screen name="Schreibtisch" component={Root} />
-        <Drawer.Screen name="Kalender" component={Root} />
-        <Drawer.Screen name="Studenplan" component={Home} />
-        <Drawer.Screen name="Wichtige Links" component={Home} />
-        <Drawer.Screen name="Fächer" component={Home} />
-        <Drawer.Screen name="Noten" component={Home} />
-        <Drawer.Screen name="Lernplan" component={Home} />
+      <Drawer.Navigator initialRouteName="HomeView">
+        <Drawer.Screen name="Schreibtisch" component={HomeView} />
+        <Drawer.Screen name="Kalender" component={CalenderView} />
+        <Drawer.Screen name="Studenplan" component={HomeView} />
+        <Drawer.Screen name="Wichtige Links" component={HomeView} />
+        <Drawer.Screen name="Fächer" component={HomeView} />
+        <Drawer.Screen name="Noten" component={HomeView} />
+        <Drawer.Screen name="Lernplan" component={HomeView} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
