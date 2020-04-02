fe22a41224797024dc45f2bfcd3893ad46c0c711
cwacker <cwacker@stud.hs-heilbronn.de>
2020-04-02 21:51:40 +0200

    add Controller classes

--- backend/src/main/java/com/group3/backend/controller/FieldOfStudyController.java
+++ backend/src/main/java/com/group3/backend/controller/FieldOfStudyController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/task")
-public class TaskController {
+@RequestMapping("/fieldOfStudy")
+public class FieldOfStudyController {
+
 }
