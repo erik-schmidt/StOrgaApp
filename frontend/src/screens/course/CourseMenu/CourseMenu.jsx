5da60f75000fb813cc03bcce60a96a3808e728ab
Erik Schmidt <erschmid@stud.hs-heilbronn.de>
2020-05-03 18:31:38 +0200

    Changed Modal to use component for consistancy - Add grade working - Missing Login with backend to add grade to course

--- frontend/src/screens/course/CourseMenu/CourseMenu.jsx
+++ frontend/src/screens/course/CourseMenu/CourseMenu.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, TouchableHighlight } from "react-native";
+import { View, TextInput } from "react-native";
 import styles from "./CourseMenu.style";
 import AppButton from "../../../components/AppButton/AppButton";
 import { deleteCourse } from "../../../api/services/courseService";
@@ -41,77 +41,65 @@ const CourseMenu = ({ navigation, route }) => {
   return (
     <View style={styles.container}>
       {editMode ? (
-        <View style={styles.modalView}>
-          <Text
-            style={{ ...styles.modalText, fontWeight: "bold", fontSize: 20 }}
-          >
-            Veranstaltung: {course.description}
-          </Text>
-          <TextInput
-            style={styles.textInput}
-            keyboardType="number-pad"
-            onChangeText={(text) => setSelectedGrade(text)}
-            value={selectedGrade}
-          />
-          <TouchableHighlight
-            style={styles.modalButton}
-            onPress={() => console.log("Ändern wurde ausgewählt")}
-          >
-            <Text style={styles.textStyle}>Speichern</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            style={{ ...styles.modalButton, marginTop: 10 }}
-            onPress={() => navigation.pop()}
-          >
-            <Text style={styles.textStyle}>Abbrechen</Text>
-          </TouchableHighlight>
+        <View>
+          <AppModal header="Note eintragen" description={course.description}>
+            <TextInput
+              style={styles.textInput}
+              keyboardType="number-pad"
+              placeholder="Note"
+              onChangeText={(text) => setSelectedGrade(text)}
+              value={selectedGrade}
+            />
+            <AppButton
+              onPress={() => console.log("Ändern ausgewählt")}
+              text="Speichern"
+            />
+            <AppButton onPress={() => navigation.pop()} text="Abbrechen" />
+          </AppModal>
         </View>
       ) : (
-        <View style={styles.modalView}>
-          <Text
-            style={{ ...styles.modalText, fontWeight: "bold", fontSize: 20 }}
-          >
-            Veranstaltung: {course.description}
-          </Text>
-          <TouchableHighlight
-            style={styles.modalButton}
-            onPress={() => onChangeGrade()}
-          >
-            <Text style={styles.textStyle}>Note ändern</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            style={{ ...styles.modalButton, backgroundColor: "#f00" }}
-            onPress={() => onDeleteCourse()}
-          >
-            <Text style={styles.textStyle}>Löschen</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            style={{ ...styles.modalButton, marginTop: 25 }}
-            onPress={() => navigation.pop()}
-          >
-            <Text style={styles.textStyle}>Abbrechen</Text>
-          </TouchableHighlight>
+        // <View style={styles.modalView}>
+        //   <Text
+        //     style={{ ...styles.modalText, fontWeight: "bold", fontSize: 20 }}
+        //   >
+        //     Veranstaltung: {course.description}
+        //   </Text>
+
+        //   <TouchableHighlight
+        //     style={styles.modalButton}
+        //     onPress={() => console.log("Ändern wurde ausgewählt")}
+        //   >
+        //     <Text style={styles.textStyle}>Speichern</Text>
+        //   </TouchableHighlight>
+        //   <TouchableHighlight
+        //     style={{ ...styles.modalButton, marginTop: 10 }}
+        //     onPress={() => navigation.pop()}
+        //   >
+        //     <Text style={styles.textStyle}>Abbrechen</Text>
+        //   </TouchableHighlight>
+        // </View>
+        <View>
+          <AppModal header="Veranstaltung:" description={course.description}>
+            <AppButton onPress={() => onChangeGrade()} text="Note ändern" />
+            <AppButton
+              color="red"
+              onPress={() => onDeleteCourse()}
+              text="Kurs löschen"
+            />
+            <AppButton onPress={() => navigation.pop()} text="Abbrechen" />
+          </AppModal>
+          <Toast
+            color="red"
+            showModal={error}
+            text="Keine Verbindung zum Server"
+          />
+          <Toast
+            color="green"
+            showModal={visible}
+            text="Kurs erfolgreich gelöscht"
+          />
         </View>
       )}
-
-      <AppModal header="Veranstaltung:" description={course.description}>
-        <AppButton
-          onPress={() => console.log("Ändern ausgewählt")}
-          text="Note ändern"
-        />
-        <AppButton
-          color="red"
-          onPress={() => onDeleteCourse()}
-          text="Kurs löschen"
-        />
-        <AppButton onPress={() => navigation.pop()} text="Abbrechen" />
-      </AppModal>
-      <Toast color="red" showModal={error} text="Keine Verbindung zum Server" />
-      <Toast
-        color="green"
-        showModal={visible}
-        text="Kurs erfolgreich gelöscht"
-      />
     </View>
   );
 };
