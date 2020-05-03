7a5635970d6dd9477ec534be35e7261db0bf033b
cwacker <cwacker@stud.hs-heilbronn.de>
2020-05-03 14:23:28 +0200

    develop punktioniert lokal

---
+++ backend/src/main/java/com/group3/backend/exceptions/CourseDescriptionAlreadyExistsException.java
@@ -0,0 +1,6 @@
+package com.group3.backend.exceptions;
+
+public class CourseDescriptionAlreadyExistsException extends Exception {
+    public CourseDescriptionAlreadyExistsException(String message){super(message);}
+}
+
--- backend/src/main/java/com/group3/backend/service/CourseService.java
+++ backend/src/main/java/com/group3/backend/service/CourseService.java
@@ -1,8 +1,12 @@
 package com.group3.backend.service;
 
+import com.group3.backend.exceptions.CourseDescriptionAlreadyExistsException;
+import com.group3.backend.exceptions.CourseNumberAlreadyExistsException;
 import com.group3.backend.model.Course;
+import com.group3.backend.model.GradeCourseMapping;
 import com.group3.backend.model.Student;
 import com.group3.backend.repository.CourseRepository;
+import com.group3.backend.repository.GradeCourseMappingRepository;
 import com.group3.backend.repository.StudentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,12 +24,14 @@ public class CourseService {
 
     private CourseRepository courseRepository;
     private StudentRepository studentRepository;
+    private GradeCourseMappingRepository gradeCourseMappingRepository;
     private Logger logger = LoggerFactory.getLogger(CourseService.class);
 
     @Autowired
-    public CourseService(CourseRepository courseRepository, StudentRepository studentRepository) {
+    public CourseService(CourseRepository courseRepository, StudentRepository studentRepository, GradeCourseMappingRepository gradeCourseMappingRepository) {
         this.courseRepository = courseRepository;
         this.studentRepository = studentRepository;
+        this.gradeCourseMappingRepository = gradeCourseMappingRepository;
     }
 
     public String ping(){
@@ -61,21 +67,69 @@ public class CourseService {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    // TODO: 24.04.2020 create course not available for interface
     public ResponseEntity<Course> createCourse(Course course){
-        Course cs = new Course(course.getFieldOfStudy(),course.getNumber(), course.getDescription(),
-                course.getRoom(), course.getProfessor(), course.getEcts(), course.getKindOfSubject(), course.getReccomendedSemester(),
-                course.getStudyFocus());
-        courseRepository.save(cs);
-        return new ResponseEntity<>(HttpStatus.OK);
+        boolean courseNumberAlreadyExists = false;
+        boolean courseDescriptionAlreadyExists = false;
+        for (Course c : courseRepository.findAll()){
+            if (course.getNumber().equals(c.getNumber())){
+                courseNumberAlreadyExists = true;
+            }
+            if (course.getDescription().equals(c.getDescription())){
+                courseDescriptionAlreadyExists = true;
+            }
+        }
+        try{
+            if (courseNumberAlreadyExists){
+                throw new CourseNumberAlreadyExistsException("Coursenumber already exists in the repository");
+            }
+            if (courseDescriptionAlreadyExists){
+                throw new CourseDescriptionAlreadyExistsException("Coursedescription already exists");
+            }
+            Course cs = new Course(course.getFieldOfStudy(),course.getNumber(), course.getDescription(),
+                    course.getRoom(), course.getProfessor(), course.getEcts(), course.getKindOfSubject(), course.getReccomendedSemester(),
+                    course.getStudyFocus());
+            courseRepository.save(cs);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        catch (CourseNumberAlreadyExistsException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        catch (CourseDescriptionAlreadyExistsException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
-    // TODO: 24.04.2020 delete course student mapping
     public Course deleteCourse(String number){
         Course course = courseRepository.findByNumber(number);
         courseRepository.delete(course);
         return course;
     }
 
+    public double getGradeByMatrNrAndCourseNumber(String matrNr, String number){
+        Student student = studentRepository.findByMatrNr(matrNr);
+        Set<GradeCourseMapping> gradeCourseMappingSet = gradeCourseMappingRepository.findAllByStudent(student);
+        double grade = 0;
+        for (GradeCourseMapping mapping : gradeCourseMappingSet){
+            if (mapping.getCourseNumber().equals(number)){
+                grade = mapping.getGrade();
+            }
+        }
+        return grade;
+    }
+
+    public Course deleteCourseFromStudent(String number, String matrNr){
+        Student student = studentRepository.findByMatrNr(matrNr);
+        Set<Course> courseSet = student.getCourses();
+        Course course = new Course();
+        for (Course c:courseSet){
+            if (c.getNumber().equals(number)){
+                course = c;
+            }
+        }
+        courseRepository.delete(course);
+        return course;
+    }
+
 
 }
+
