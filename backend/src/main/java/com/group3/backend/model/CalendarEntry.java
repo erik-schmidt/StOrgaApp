543f998e9b02e6169e4372ed8b58d5bb3d1d7003
cwacker <cwacker@stud.hs-heilbronn.de>
2020-05-06 17:57:50 +0200

    bugfix calender develop

--- backend/src/main/java/com/group3/backend/model/CalendarEntry.java
+++ backend/src/main/java/com/group3/backend/model/CalendarEntry.java
@@ -1,54 +1,62 @@
 package com.group3.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.apache.tomcat.jni.Local;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
-import java.sql.Date;
-import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.time.LocalTime;
 
 
 @Entity(name = "CalendarEntry")
 public class CalendarEntry {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Integer id;
 
     private String name;
 
     // TODO: 24.04.2020 Eventuell stimmen die Datentypen für die folgenden 3 Attribute nicht.
     @Column(columnDefinition = "TIME")
-    @Type(type="org.hibernate.type.TIME")
-    @Convert(disableConversion = true)
-    private Time entryStartTime;
+   // @Convert(disableConversion = true)
+    private LocalTime entryStartTime;
 
     @Column(columnDefinition = "TIME")
-    @Type(type="org.hibernate.type.TIME")
-    @Convert(disableConversion = true)
-    private Time entryFinishTime;
+    //@Convert(disableConversion = true)
+    private LocalTime entryFinishTime;
 
     @Column(columnDefinition = "DATE")
-    @Type(type="org.hibernate.type.DATE")
-    @Convert(disableConversion = true)
-    private Date entryDate;
+    //@Convert(disableConversion = true)
+    private LocalDate entryDate;
 
     private String description;
 
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "student")
+    //@JoinColumn(name = "student")
     private Student student = new Student();
 
     public CalendarEntry() {
 
     }
 
-    public CalendarEntry(String name, Time entryStartTimeString, Time entryFinishTime, Date entryDate, String description){
+    public CalendarEntry(String name, LocalTime entryStartTimeString, LocalTime entryFinishTime, LocalDate entryDate, String description){
         this.name = name;
         this.entryStartTime = entryStartTimeString;
         this.entryFinishTime = entryFinishTime;
         this.entryDate = entryDate;
         this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -59,27 +67,27 @@ public class CalendarEntry {
         this.name = name;
     }
 
-    public Time getEntryStartTime() {
+    public LocalTime getEntryStartTime() {
         return entryStartTime;
     }
 
-    public void setEntryStartTime(Time entryStartTime) {
+    public void setEntryStartTime(LocalTime entryStartTime) {
         this.entryStartTime = entryStartTime;
     }
 
-    public Time getEntryFinishTime() {
+    public LocalTime getEntryFinishTime() {
         return entryFinishTime;
     }
 
-    public void setEntryFinishTime(Time entryFinishTime) {
+    public void setEntryFinishTime(LocalTime entryFinishTime) {
         this.entryFinishTime = entryFinishTime;
     }
 
-    public Date getEntryDate() {
+    public LocalDate getEntryDate() {
         return entryDate;
     }
 
-    public void setEntryDate(Date entryDate) {
+    public void setEntryDate(LocalDate entryDate) {
         this.entryDate = entryDate;
     }
 
--- backend/src/main/java/com/group3/backend/repository/CalendarEntryRepository.java
+++ backend/src/main/java/com/group3/backend/repository/CalendarEntryRepository.java
@@ -2,6 +2,7 @@ package com.group3.backend.repository;
 
 import com.group3.backend.model.CalendarEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
@@ -11,11 +12,13 @@ import java.util.Set;
 
 @Repository
 public interface CalendarEntryRepository extends JpaRepository<CalendarEntry, String> {
-    CalendarEntry findByStudAndId(String matrNr, int id);
+    //@Query(value = "SELECT * FROM CalendarEntry WHERE "
+    //CalendarEntry findByStudAndId(String matrNr, int id);
+    CalendarEntry findByDescription(String description);
+    List<CalendarEntry> findAllByStudentId(Integer id);
+    //CalendarEntry findByDescription(String matrNr, String description);
 
-    CalendarEntry findByDescription(String matrNr, String description);
-
-    List<CalendarEntry> findByDate(String matrNr, Date date);
+    //List<CalendarEntry> findByDate(String matrNr, Date date);
 
     // TODO: 24.04.2020 : getAllEntriesOfWeek und getAllEntriesOfMonth damit die DB nicht überlastet wird. Dementsprechend wird dateOfEntry zu einem Datumsobjekt
     // TODO: 24.04.2020 : Tests schreiben
