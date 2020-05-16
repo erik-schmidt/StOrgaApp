40fcfbb9b8a4459f251544340370416e93ed3e90
Erik <erschmid@stud.hs-heilbronn.de>
2020-05-16 15:04:33 +0200

    Auth fix

--- authentication-server/src/main/java/com/group3/authentication/authenticationserver/AuthenticationController.java
+++ authentication-server/src/main/java/com/group3/authentication/authenticationserver/AuthenticationController.java
@@ -4,13 +4,11 @@ import com.group3.authentication.authenticationserver.model.Student;
 import com.group3.authentication.authenticationserver.request.AuthenticationRequest;
 import com.group3.authentication.authenticationserver.service.AuthenticationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
+@CrossOrigin
 public class AuthenticationController {
 
     private AuthenticationService authenticationService;
