75f2aa92c71a6e895bb2e7ad360398a1e1fcc28c
cwacker <cwacker@stud.hs-heilbronn.de>
2020-06-09 21:11:28 +0200

    fix timetable faults

--- backend/src/main/java/com/group3/backend/dataHandling/DataHandler.java
+++ backend/src/main/java/com/group3/backend/dataHandling/DataHandler.java
@@ -2,6 +2,7 @@ package com.group3.backend.dataHandling;
 
 import com.group3.backend.model.Course;
 import com.group3.backend.model.Student;
+import com.group3.backend.model.TimeTableObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,12 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.*;
 
 public class DataHandler {
 
@@ -22,6 +27,7 @@ public class DataHandler {
     //private final String AIBCOURSES_FILE = "AIBCoursesSPO.txt";
     private final String AIBCOURSES_FILE = "AIBCoursesSPOEnlarged.txt";
     private final String ADMIN_USER = "AdminUser.txt";
+    private final String TIMETABLE = "splan_ss_2020_AIB.txt";
     private Logger logger = LoggerFactory.getLogger(DataHandler.class);
 
     public DataHandler(){
@@ -78,5 +84,90 @@ public class DataHandler {
             logger.error("Error wihile loading inital course data: " + e.getClass() + " " + e.getMessage());
         }
         return null;
+    }
+
+    /**
+     * loads the time table objects out of the timetable file wich can be downloaded for AIB4 at
+     * https://splan.hs-heilbronn.de/splan/ical?lan=de&puid=29&type=pg&pgid=17357
+     * @return List<TimeTableObject> or null
+     */
+    public List<TimeTableObject> loadTimeTable(){
+        List<TimeTableObject> timeTableObjectArrayList = new ArrayList<>(2000);
+        try(BufferedReader reader = new BufferedReader(new FileReader(TIMETABLE))){
+            String line = reader.readLine();
+            TimeTableObject timeTableObject = null;
+            boolean wordStart = false;
+            while (line!=null) {
+                switch (line){
+                    case "BEGIN:VEVENT":
+                        wordStart = true;
+                        timeTableObject = new TimeTableObject();
+                        break;
+                    case "END:VEVENT":
+                        timeTableObjectArrayList.add(timeTableObject);
+                        wordStart = false;
+                        break;
+                    default:
+                        if(wordStart) {
+                            timeTableObject = fillTimeTableObjectWithAttributes(timeTableObject, line);
+                        }
+                        break;
+                }
+                line = reader.readLine();
+            }
+            return timeTableObjectArrayList;
+        }catch (Exception e){
+            logger.error("Error wihile loading inital time table data: " + e.getClass() + " " + e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * logic of fill the time table objects with values
+     * @param timeTableObject new time table object which has to be filled
+     * @param line current read line
+     * @return TimeTableObject with values or default time table object
+     */
+    private TimeTableObject fillTimeTableObjectWithAttributes(TimeTableObject timeTableObject, String line){
+        if(line.contains("DTSTAMP")){
+            return timeTableObject;
+        }else if(line.contains("DTSTART")){
+            String dateTime = line.substring(26);
+            String[] seperateDateTime = dateTime.split("T");
+            DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE;
+            LocalDate date = LocalDate.parse(seperateDateTime[0].replace(":", ""),formatter);
+            timeTableObject.setDate(date);
+            DateTimeFormatter timef = DateTimeFormatter.ofPattern("HH:mm:ss");
+            LocalTime startTime = LocalTime.parse(seperateDateTime[1].substring(0,2)+":"
+                    +seperateDateTime[1].substring(2,4)+":"+seperateDateTime[1].substring(4),timef);
+            timeTableObject.setStartTime(startTime);
+        }else if(line.contains("DTEND")){
+            String dateTime = line.substring(24);
+            String[] seperateDateTime = dateTime.split("T");
+            DateTimeFormatter timef = DateTimeFormatter.ofPattern("HH:mm:ss");
+            LocalTime endTime = LocalTime.parse(seperateDateTime[1].substring(0,2)+":"
+                    +seperateDateTime[1].substring(2,4)+":"+seperateDateTime[1].substring(4),timef);
+            timeTableObject.setFinishTime(endTime);
+        }else if(line.contains("SUMMARY")){
+            String summary = line.substring(8);
+            timeTableObject.setSummary(summary);
+            if(line.contains("(")){
+                String[] courseNumber = summary.split("\\(");
+                timeTableObject.setCourseNumber(courseNumber[1].replace(")", ""));
+            }
+        }else if(line.contains("UID")){
+            String uid = line.substring(4);
+            timeTableObject.setUID(uid);
+        }else if(line.contains("LOCATION")){
+            String location = line.substring(9);
+            timeTableObject.setLocation(location);
+        }else if(line.contains("DESCRIPTION")){
+            String[] description = line.replace("\\", "#").substring(12).split("#n");
+            timeTableObject.setDescription(line.substring(12).replace("\\", "#").replace("#n", " "));
+            // TODO: 30.05.2020 keine hardgecodeden AIB felder
+            if(description[description.length-1].contains("AIB"))
+            timeTableObject.setFieldOfStudySemester(description[description.length-1]);
+        }
+        return timeTableObject;
     }
 }
