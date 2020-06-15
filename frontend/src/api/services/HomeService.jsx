6b24c44232cbba69c850a56e98422a07523bf5cd
Erik <erschmid@stud.hs-heilbronn.de>
2020-06-15 08:27:37 +0200

    Add Homescreen Screen - showing the latest things - Bug fixing and performance improvements

--- frontend/src/api/services/HomeService.jsx
+++ frontend/src/api/services/HomeService.jsx
@@ -1,1 +1,8 @@
+import React from "react";
+import { fetch } from "../apiService";
+import { AsyncStorage } from "react-native";
 
+export const getHomescreenItems = async () => {
+  const matrNr = await AsyncStorage.getItem("matrNr");
+  return fetch(`homescreen/getHomescreen/${matrNr}`);
+};
