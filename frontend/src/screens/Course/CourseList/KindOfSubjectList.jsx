7ab6464d1300cb5535029d4fd0856fb317f84d58
Erik <erschmid@stud.hs-heilbronn.de>
2020-06-16 08:32:14 +0200

    Added Tab Bar to Courses for Kind of Subject and Recommended Semester - CourseList can now be viewed with major studies and extra courses which the student signed up to - Fixed Bugs

---
+++ frontend/src/screens/Course/CourseList/KindOfSubjectList.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, RefreshControl, Alert } from "react-native";
+import { getCoursesByKindOfSubject } from "../../../api/services/CourseService";
+import { FlatList } from "react-native-gesture-handler";
+import Card from "../../../components/Card/Card";
+import styles from "./CourseList.style";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import * as HttpStatus from "http-status-codes";
+import AuthContext from "../../../constants/AuthContext";
+
+const KindOfSubjectList = () => {
+  const [courses, setCourses] = useState([]);
+  const navigation = useNavigation();
+  const route = useRoute();
+  const [refreshing, setRefreshing] = useState(false);
+  const { signOut } = React.useContext(AuthContext);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    getCoursesByKindOfSubject()
+      .then((res) => {
+        if (res.status === HttpStatus.OK) {
+          setCourses(res.data);
+          setRefreshing(false);
+        } else if (res.status === HttpStatus.UNAUTHORIZED) {
+          signOut();
+        } else {
+          throw new Error(res.data);
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  useEffect(() => {
+    getCoursesByKindOfSubject()
+      .then((res) => {
+        if (res.status === HttpStatus.OK) {
+          setCourses(res.data);
+        } else if (res.status === HttpStatus.UNAUTHORIZED) {
+          signOut();
+        } else {
+          throw new Error(res.data);
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    getCoursesByKindOfSubject()
+      .then((res) => {
+        if (res.status === HttpStatus.OK) {
+          setCourses(res.data);
+        } else if (res.status === HttpStatus.UNAUTHORIZED) {
+          signOut();
+        } else {
+          throw new Error(res.data);
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }, [route]);
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={courses}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+        renderItem={({ item }) => (
+          <Card
+            key={courses.length}
+            onLongPress={() =>
+              navigation.navigate("CourseMenu", { course: item })
+            }
+            onPress={() =>
+              navigation.navigate("CourseInformationModal", { course: item })
+            }
+          >
+            <View>
+              <Text style={styles.courseHeader}>Veranstaltung: </Text>
+              <Text style={styles.courseDescription}>{item.description}</Text>
+            </View>
+            <View style={styles.cardText}>
+              <Text style={styles.boldText}>ECTS: </Text>
+              <Text>{item.ects}</Text>
+            </View>
+            <View style={styles.cardText}>
+              <Text style={styles.boldText}>Empfohlenes Semester: </Text>
+              <Text>{item.recommendedSemester}</Text>
+            </View>
+          </Card>
+        )}
+        keyExtractor={(item) => item.description}
+      />
+    </View>
+  );
+};
+
+export default KindOfSubjectList;
